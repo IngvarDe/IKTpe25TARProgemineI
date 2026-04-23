a0ddf052b89aed739de3b648e19d0654457d0536
IngvarDe <32929800+IngvarDe@users.noreply.github.com>
2026-04-23 14:53:32 +0300

    StudentUpdateGet

--- University/University/University/Controllers/StudentController.cs
+++ University/University/University/Controllers/StudentController.cs
@@ -124,5 +124,38 @@ namespace University.Controllers
             }
             return View(vm);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var student = await _context.Students
+                //Include lubab objekti kasutada objekti sees
+                .Include(s => s.Enrollments)
+                    //kui tahad uuesti objekti kasutada objekti sees, siis kasutad ThenInclude
+                    .ThenInclude(e => e.Course)
+                //andmeid ei salvestata vahemällu ja ei jälgita
+                .AsNoTracking()
+                //tagastab esimese elemendi andmetest, mis on tingimuses välja toodud
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+
+            //kui sutudent on null, siis on NotFound()
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new StudentUpdateViewModel
+            {
+                Id = student.Id,
+                FirstMidName = student.FirstMidName,
+                LastName = student.LastName,
+                EnrollmentDate= student.EnrollmentDate
+            };
+
+            //tuleb teha domaini modelist andmete ülekanne view modeli omasse
+
+            return View(vm);
+        }
     }
 }
